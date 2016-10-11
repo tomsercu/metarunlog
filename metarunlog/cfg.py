f6d94d01614d10816ad3166ed1b133f838cb2d5b
Tom Sercu <tom.sercu@gmail.com>
2016-10-11 14:46:09 -0400

    change extension of mrl_*.py to mrl_*.py.example to avoid it being picked up

--- metarunlog/cfg.py
+++ metarunlog/cfg.py
@@ -10,14 +10,14 @@ subExpFormat    = '{subExpId:03d}'
 gitFailUntrackedDefault= 'no'
 copyFiles       = ['conf.lua']
 confTemplFile   = 'conf.lua' # what file is expanded into template with makebatch()
-giturl          = 'https://github.rtp.raleigh.ibm.com/multimodal/multilingconv'
+giturl          = 'git@github.rtp.raleigh.ibm.com:multimodal/multilingconv.git'
 analysis_overview = { # {funcname: ('extra_arg1', extrarg2, ), .. }
     'bestPerf': (),
 }
 analysis_subexp = {
     'plotSinglePerf': (),
 }
-analysis_webdir = '/u/goelv/tsercu/work/002/analysis'
+analysis_webdir = '/u/tomsercu/www'
 hooks = {
     'after_new' : {},
     'after_makebatch' : {},
